864434fde8f55b443750cb71ee023e11fd336199
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-05-07 05:53:22 -0500

    initial

---
+++ src/context.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const state = {
+number : 0
+}
+
+//initialize the state value
+const NumberContext = React.createContext(state.countnumber);
+
+
+export default NumberContext
