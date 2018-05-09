429dce1be2a2c073ce5bf03f38b4e09df4402de5
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-05-08 19:03:39 -0500

    modified state names in context.js

--- src/context.js
+++ src/context.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+//set the state
 const state = {
 number : 0
 }
 
 //initialize the state value
-const NumberContext = React.createContext(state.countnumber);
+const NumberContext = React.createContext(state.number);
 
 
-export default NumberContext
+export default NumberContext;
